c1f1828a8583cb7ec93f7e942ef480adcc25054b
Justin Henderson <jhenderson@tekrefresh.com>
2018-03-31 08:26:42 -0500

    added esisode 1

---
+++ 1 - Zero Trust/zero_trust_triggers.ps1
@@ -0,0 +1,23 @@
+﻿# Example of risky/odd behavior number 1
+Clear-EventLog -LogName Security
+
+# Example of risky/odd behavior number 2
+Resolve-DnsName -Name 1abmeinc.com
+
+# Example of risky/odd behavior number 3
+$port = 445
+$systems = @("192.168.2.101","192.168.2.102","192.168.2.108","192.168.2.21","192.168.2.199")
+$repeat = 100
+$count = 0
+while($count -ne $repeat){
+    foreach($system in $systems){
+        $socket = new-object System.Net.Sockets.TcpClient($system, $port)
+        if($socket.Connected){
+            Write-Host "Port $port is open on $system" -ForegroundColor Cyan
+            $socket.Close()
+        } else {
+            Write-Host "Port $port is closed on $system" -ForegroundColor Red
+        }
+    }
+    $count++
+}
